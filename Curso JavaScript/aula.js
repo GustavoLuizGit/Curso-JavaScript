81d7a763b7425125b99f07657e8a5cf39611f4e5
GustavoLuizGit <glogustavo4321@gmail.com>
2021-12-04 08:44:32 -0300

    Update js and html
    Código

---
+++ Curso JavaScript/aula.js
@@ -0,0 +1,43 @@
+//AULA 1 INICIO
+// let idade = 5;
+// console.log(idade);
+// console.log(idade + 1);
+// let altura = 180;
+// console.log(altura);
+// let altura = 180;
+// let nome = 'Gustavo';
+// let idade = 21;
+// console.log('O nome é ',nome,'e ele tem',idade,'anos e ',altura,'de altura');
+//AULA 1 FIM
+
+//INICIO AULA 2
+// const valorIngressoAdulto = 20;
+// console.log(valorIngressoAdulto);
+//FIM AULA 2 
+
+//INICIO AULA 3
+// let nome = 'Gustavo'; //string literal
+// let idade = 25; //number literal
+// let statusAprovado = true; //boolean
+// let sobrenome = undefined // nada comum fazer isso, sem tipo
+// let corSelecionado = null; //null é utilizado quando voce quer resetar um valor
+//FIM AULA 3
+
+//INICIO AULA 4
+// let nome = 'Maria';
+// let idade = 21;
+// let anoNascimento = 2000;
+// let sobrenome = 'Oliveira';
+// let statusAprovado = true;
+
+// let pessoa = {
+//     nome : 'Gustavo',
+//     idade : 30,
+//     anoNascimento : 2000,
+//     statusAprovado : true
+// };
+// console.log(pessoa);
+//FIM AULA 4
+
+//INICIO AULA 5
+//FIM AULA 5
