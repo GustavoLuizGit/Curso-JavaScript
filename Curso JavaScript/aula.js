831dca5a6e52a48da63d7201686cbd44a9ea2e7f
GustavoLuizGit <glogustavo4321@gmail.com>
2021-12-08 12:18:27 -0300

    Update aula.js
    Comando IF, elseif, else

--- Curso JavaScript/aula.js
+++ Curso JavaScript/aula.js
@@ -153,3 +153,16 @@
 // console.log(a);
 // console.log(b);
 //Fim aula 9
+
+//Aula 10 - IF
+let hora = 06;
+if (hora >=0 && hora<=12){
+    console.log("Bom dia !");
+}
+else if (hora >= 12 && hora<=18){
+    console.log("Boa tarde");
+}
+else{
+    console.log("Boa noite");
+}
+//Fim aula 19
